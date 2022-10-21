795a0c2b56062ef94c2ac581e63d795873083402
HoVeRzzz <103826368+HoVeRzzz@users.noreply.github.com>
2022-10-22 00:52:02 +0100

    Add files via upload

---
+++ nvim/plugin/lsp.lua
@@ -0,0 +1,26 @@
+lsp = require("lspconfig")
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+
+local servers = { 'gopls', 'clangd', 'pyright' }
+
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set('n', '<leader>fm', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
+
+end
+
+
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
