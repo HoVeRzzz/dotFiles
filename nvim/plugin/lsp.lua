18d5abee94304a9d983fb800a318c20118d312c7
HoVeRzzz <103826368+HoVeRzzz@users.noreply.github.com>
2022-11-07 20:49:05 +0000

    added html and css

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -3,7 +3,7 @@ lsp = require("lspconfig")
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 
-local servers = { 'gopls', 'clangd', 'pyright' }
+local servers = { 'gopls', 'clangd', 'pyright', 'html', 'cssls'}
 
 local on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
